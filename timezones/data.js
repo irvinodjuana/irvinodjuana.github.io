9de92c0dbdfaa2888a47e32265bcf63da4af3c99
Irvino Djuana <irvino.djuana@gmail.com>
2022-07-01 22:04:36 -0500

    Add Jakarta timezone

--- timezones/data.js
+++ timezones/data.js
@@ -2,5 +2,6 @@ var data = [
     [-05,"Chicago","CDT UTC-05",true],
     [-07,"Vancouver","PDT UTC-07",true],
     [-04,"Montreal","EDT UTC-04",true],
+    [+07, "Jakarta", "WIB UTC+07",true]
     [+0,"UTC","UTC+00",true],
 ];
